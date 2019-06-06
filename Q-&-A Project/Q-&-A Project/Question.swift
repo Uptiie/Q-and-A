158ac8ff032d3f01a40c0ebf59f525a856f5ada0
Donovan Langager <donovan.lan16@gmail.com>
2019-06-06 16:39:14 -0700

    Fairly behind on todays project, should be done by tomorrow

---
+++ Q-&-A Project/Q-&-A Project/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  Q-&-A Project
+//
+//  Created by Uptiie on 6/6/19.
+//  Copyright © 2019 Uptiie. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let question: String
+    let asker: String?
+    let answerer: String?
+}
